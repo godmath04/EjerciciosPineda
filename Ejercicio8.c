af8097a57e44abacb8005b365203a56ac3f185eb
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 14:18:38 -0500

    Ej8. Correcion, antes mostrada desde el 2, ahora muestra desde el 20

--- Ejercicio8.c
+++ Ejercicio8.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-// EL PROGRAMA DEBE MOSTRAR LOS NUMEROS PARES ENTRE2 Y 200 
+// EL PROGRAMA DEBE MOSTRAR LOS NUMEROS PARES ENTRE2 Y 200
 int main()
 {
-    int minimo = 2;
+    int minimo = 20;
     int maximo = 200;
-    int x = 2; 
+    int x = 2;
     int pares;
     printf("EL PROGRAMA MUESTRA LOS NUMEROS PARES ENTRE 20 Y 200: \n");
     do
     {
-        pares = x%2;
+        pares = x % 2;
         if (pares == 0)
         {
-           printf("%d ", x);
+            printf("%d ", x);
         }
         x++;
-    } while (x<=maximo && x>=minimo);
+    } while (x <= maximo && x >= minimo);
 
     return 0;
 }
