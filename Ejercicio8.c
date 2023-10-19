d69c24bc56498dbcacbaf3207bb29e4f8f63de68
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 14:15:36 -0500

    Ej8 Bucle para mostrar los numeros primos

--- Ejercicio8.c
+++ Ejercicio8.c
@@ -4,6 +4,16 @@ int main()
     int minimo = 2;
     int maximo = 200;
     int x = 2; 
-    int pares; 
+    int pares;
+    do
+    {
+        pares = x%2;
+        if (pares == 0)
+        {
+           printf("%d ", x);
+        }
+        x++;
+    } while (x<=maximo && x>=minimo);
+
     return 0;
 }
