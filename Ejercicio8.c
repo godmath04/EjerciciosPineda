bdfcdf94b2299ae81bd5102224abe911b21d41cf
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 14:21:34 -0500

    Ej8. Finalizado, el programa cumple y los comentarios son necesarios

--- Ejercicio8.c
+++ Ejercicio8.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-// EL PROGRAMA DEBE MOSTRAR LOS NUMEROS PARES ENTRE2 Y 200
+// EL PROGRAMA DEBE MOSTRAR LOS NUMEROS PARES ENTRE 20 Y 200
 int main()
 {
     int minimo = 20;
     int maximo = 200;
-    int x = 2;
+    int x = 20; 
     int pares;
     printf("EL PROGRAMA MUESTRA LOS NUMEROS PARES ENTRE 20 Y 200: \n");
+    // La x recorre todos los numeros obteniendo los pares
     do
     {
         pares = x % 2;
@@ -16,7 +17,7 @@ int main()
             printf("%d ", x);
         }
         x++;
-    } while (x <= maximo && x >= minimo);
+    } while (x <= maximo && x >= minimo); // Se realiza mientras nuestra x este entre el minimo y el maximo
 
     return 0;
 }
