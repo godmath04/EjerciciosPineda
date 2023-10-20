e586e43ca0b2bf3863611758f86dc1b356b2d913
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-20 11:42:03 -0500

    Ej38 Finalizado, todo funciona correctamente

--- Ejercicio38.c
+++ Ejercicio38.c
@@ -8,22 +8,30 @@ int main()
     printf("Ingrese el numero para el cual desea realizar la tabla de multiplicar: \n");
     scanf("%d", &numero);
 
-    // Titulo de las tablas
-    for (int i = 1; i <= numero; i++)
+    if (numero <= 0)
     {
-        printf("%2d |", i);
+        printf("Su numero es menor o igual a cero, la tabla se muestra solo para positivos\n");
+            printf("Si desea probar con un negativo aplique la ley de signos.\n");
     }
-    printf("\n\n");
-
-    // Bucle de multiplicacion que crea el contenido de la tabla
-    for (int i = 1; i <= numero; i++)
+    else
     {
-        for (int j = 1; j <= numero; j++)
+
+        // Titulo de las tablas
+        for (int i = 1; i <= numero; i++)
         {
-            printf("%2d |", i * j); // El %2d hace que haya un doble espacio de caracteres para crear unifromidad
+            printf("%4d |", i);
         }
         printf("\n\n");
-    }
 
+        // Bucle de multiplicacion que crea el contenido de la tabla
+        for (int i = 1; i <= numero; i++)
+        {
+            for (int j = 1; j <= numero; j++)
+            {
+                printf("%4d |", i * j); // El %2d hace que haya un doble espacio de caracteres para crear unifromidad
+            }
+            printf("\n\n");
+        }
+    }
     return 0;
 }
