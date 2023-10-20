6fae23840e11ce213cb70aba35aa9f5d8badf01f
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 22:39:06 -0500

    Ej18 Tratando de arreglar el git push

--- Ejercicio18.c
+++ Ejercicio18.c
@@ -1,18 +1,15 @@
 /* LEER DOS NUMEROS ENTEROS Y MOSTRAR TODOS LOS NUMEROS MULTIPLOS DE 5
 COMPRENDIDOS ENTRE EL MAYOR Y EL MANO*/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int num1, num2;
     int MULTIPLO = 5;
     int x, menor, mayor;
+    printf("PROGRAMA QUE LEE DOS NUMEROS Y MUESTRA LOS MULTIPLOS DE 5 ENTRE ELLOS: \n");
+    printf("Ingrese dos numeros para obtener los multiplos de 5 entre ellos: \n");
 
-
-
-
-
-    
     return 0;
 }
