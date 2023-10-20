80fa1d018d91cf7528bfac3beb506e96638e9e58
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-20 11:33:14 -0500

    Ej38 Se agrego una linea y la separacion adecuada para que sea mas visible

--- Ejercicio38.c
+++ Ejercicio38.c
@@ -4,11 +4,19 @@
 int main()
 {
     int numero = 10;
+    // Titulo de las tablas
+    for (int i = 0; i <= numero; i++)
+    {
+        printf("%2d |", i);
+    }
+    printf("\n\n");
+
+    // Bucle de multiplicacion que crea el contenido de la tabla
     for (int i = 1; i <= numero; i++)
     {
-        for (int j = 0; j <= numero; j++)
+        for (int j = 1; j <= numero; j++)
         {
-            printf("%2d *%2d=%2d   ", i, j, i * j); // El %2d hace que haya un doble espacio de caracteres para crear unifromidad
+            printf("%2d |", i*j); // El %2d hace que haya un doble espacio de caracteres para crear unifromidad
         }
         printf("\n\n");
     }
