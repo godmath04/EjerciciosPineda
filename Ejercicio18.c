3f8a158f33917a0e782ec5b27350fc11cf2383b6
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 23:50:53 -0500

    Ej18 Final del Ejercicio Se agrego el bucle do while y se hizo los ajustes finales

--- Ejercicio18.c
+++ Ejercicio18.c
@@ -7,20 +7,19 @@ int main()
 {
     int num1;
     int num2;
-    int MULTIPLO = 5;
     int x;
     int menor, mayor;
 
     printf("PROGRAMA QUE LEE DOS NUMEROS Y MUESTRA LOS MULTIPLOS DE 5 ENTRE ELLOS: \n");
-    printf("Ingrese dos numeros para obtener los multiplos de 5 entre ellos: \n");
-    scanf("%d", &num1);
+    printf("Ingrese dos numeros separados por un espacio para obtener los multiplos de 5 entre ellos: \n");
+    scanf("%d %d", &num1, &num2);
     printf("\n");
-    scanf("%d", &num2);
 
-    printf("Sus numeros son %d y %d", num1, num2); //Se lee los datos y almacena en num1 y num2
+    printf("Sus numeros son %d y %d", num1, num2); // Se lee los datos y almacena en num1 y num2
     printf("\n");
     printf("Los multiplos de 5 son: \n");
-    if (num1 < num2) // Se verifica cual numero ingresado es el menor, mayor y se les asigna una variable 
+
+    if (num1 < num2) // Se verifica cual numero ingresado es el menor, mayor y se les asigna una variable
     {
         menor = num1;
         mayor = num2;
@@ -30,7 +29,15 @@ int main()
         menor = num2;
         mayor = num1;
     }
-    
+    x = menor; // Hace un recorrido desde el menor numero obteniendo todos los valores multiplos
+    do
+    {
+        if (x % 5 == 0)
+        {
+            printf("%d ", x);
+        }
+        x++;
+    } while (x >= menor && x <= mayor); //La condicion del recorrido de la x 
 
     return 0;
 }
