980d7d2b4fe4c5bbe151ea783df4fd268153ae19
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 23:03:37 -0500

    Ej18 se trabajo en la recepcion de datos y compro que las variables no afectan el resto del codigo (errores de novato)

--- Ejercicio18.c
+++ Ejercicio18.c
@@ -5,11 +5,18 @@ COMPRENDIDOS ENTRE EL MAYOR Y EL MANO*/
 
 int main()
 {
-    int num1, num2;
+    int num1;
+    int num2;
     int MULTIPLO = 5;
-    int x, menor, mayor;
+    int x;
+    int menor, mayor;
+
     printf("PROGRAMA QUE LEE DOS NUMEROS Y MUESTRA LOS MULTIPLOS DE 5 ENTRE ELLOS: \n");
     printf("Ingrese dos numeros para obtener los multiplos de 5 entre ellos: \n");
+    scanf("%d", &num1);
+    printf("\n");
+    scanf("%d", &num2);
 
+    printf("Sus numeros son %d y %d", num1, num2);
     return 0;
 }
