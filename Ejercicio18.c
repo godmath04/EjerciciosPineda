ba756b5752b524d85f416b696e32e03f8a6659fd
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-19 22:33:53 -0500

    Ej18 Declaracion de variables

---
+++ Ejercicio18.c
@@ -0,0 +1,18 @@
+/* LEER DOS NUMEROS ENTEROS Y MOSTRAR TODOS LOS NUMEROS MULTIPLOS DE 5
+COMPRENDIDOS ENTRE EL MAYOR Y EL MANO*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num1, num2;
+    int MULTIPLO = 5;
+    int x, menor, mayor;
+
+
+
+
+
+    
+    return 0;
+}
