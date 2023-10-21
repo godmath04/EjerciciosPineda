0aaaaedaacef8dfad134415c9bc348a58eece4d6
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-20 20:26:34 -0500

    Ej48. Se plantea la primera solucion para imprimir los numeros pares

---
+++ Ejercicio48.c
@@ -0,0 +1,21 @@
+// Utilizando ciclos anidados generar las siguientes parejas de enteros
+#include <stdio.h>
+int main()
+{
+    int num1;
+    int num2;
+
+    // PRIMERO LOS NUMEROS PARES DE LA COL.IZQUIERDA VAN AVANZADO DE 1 EN 1 CON LOS DE LA COL DERECHA
+    // EN OTRAS PALABRAS LA COL DE LA DERECHA ES LA MITAD DE LOS PARES SUMADO 1
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            num2 = i/2 +1;
+        printf("%d %d\n", i, num2);
+        }
+        
+    }
+    
+    return 0;
+}
